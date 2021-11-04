dc6afdda64320395821fbe5c88156f315a342498
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-11-04 11:30:17 +0100

    added new post route. It will match incoming values from frontend with values in db

--- routes/session.js
+++ routes/session.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const Session = require('../models/session');
+const User = require('../models/user');
 
 
 /*Add new sessions*/
 router.post('/add', function (req, res, next) {
-  const { userId, sessionId, location, time, users } = req.body;
+  const { userId, sessionId, location, street, time, users } = req.body;
 
   const session = new Session({
     userId,
     sessionId,
     location,
+    street,
     time,
     users
   })
@@ -31,6 +33,25 @@ router.get('/', function (req, res, next) {
       res.json(session);
     })
 });
+
+
+// Add session to db
+router.post('/add', (req, res, next) => {
+
+  // Receive username and sessionId
+
+  const { userName, sessionId } = req.body;
+
+
+
+  // med sessionId matchar jag det id:et med databas sessionid
+
+
+  // uppdatera arrayen med users och lägg till username där om det inte finns ett sånt username sen tidigare
+
+})
+
+
 
 module.exports = router;
 
