a61b4467f1f1764823e30d6ed335d2794a0e370e
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-11-15 18:07:29 +0100

    fixes join functionality from frontend

--- routes/session.js
+++ routes/session.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const Session = require('../models/session');
-const User = require('../models/user');
 
 
 /*Add new sessions*/
@@ -39,47 +38,24 @@ router.get('/', (req, res, next) => {
 
 
 // Check sessionId and compare with sessionId to db
+
 router.post('/check', (req, res, next) => {
 
-  // Receive username and sessionId
+  Session.findOne({ sessionId: req.body.sessionId }).exec().
+    then(doc => {
+      console.log(doc);
 
-  // const { userName, sessionId } = req.body;
-
-  // med sessionId matchar jag det id:et med databas sessionid
-
-  Session.find({ sessionId: req.body.sessionId }, (err, sessions) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('sessions', sessions[0]);
-      sessions[0].users.push(req.body.userName);
-      console.log('NYA', sessions);
-      res.json(sessions);
-    }
-  })
-  // .
-  //   then(session => {
-  //     console.log(session);
-  //     console.log(sessionId);
-  //   })
+      doc.users.push(req.body.userName);
+      doc.save()
+      res.json({ participants: doc.users });
+    })
 
 
-  // uppdatera arrayen med users och lägg till username där om det inte finns ett sånt username sen tidigare
 
 })
 
 
 
+
 module.exports = router;
 
-
-// post fetch
-
-// username
-// sessionId
-
-// session / join
-
-// find the session id and match it
-
-// uppdate array in db with the username from the post
